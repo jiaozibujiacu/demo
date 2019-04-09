ad57be2ea9e5539150e0935b4cdb5198eb37ed46
xiam <xiam@gzsendi.cn>
2019-04-09 15:32:06 +0800

    测试

---
+++ src/main/java/com/example/demo/service/TestService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+/**
+ * @Description: 测试
+ * @Author: xiam
+ * @Create: 2019-04-09
+ **/
+public class TestService {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
